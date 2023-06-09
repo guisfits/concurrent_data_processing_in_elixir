cde0726ef915d8e0f04dcd39eeafee54c9cb17f1
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 18:20:31 -0300

    edit PageProducer

--- 05_data-ingestion_pipelines_with_broadway/scraper/lib/page_producer.ex
+++ 05_data-ingestion_pipelines_with_broadway/scraper/lib/page_producer.ex
@@ -3,18 +3,16 @@ defmodule PageProducer do
   use GenStage
   require Logger
 
-  def start_link(_arsg) do
-    initial_state = []
-    GenStage.start_link(__MODULE__, initial_state, name: __MODULE__)
-  end
-
   def init(initial_state) do
     Logger.info("PageProducer init")
     {:producer, initial_state}
   end
 
   def scrape_pages(pages) when is_list(pages) do
-    GenStage.cast(__MODULE__, {:pages, pages})
+    ScrapingPipeline
+    |> Broadway.producer_names()
+    |> List.first()
+    |> GenStage.cast({:pages, pages})
   end
 
   def handle_cast({:pages, pages}, state) do
