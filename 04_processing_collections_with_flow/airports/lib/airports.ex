5b0a56a79eeca5315dec8a55436691cecd770f06
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 14:29:23 -0300

    some new functions

--- 04_processing_collections_with_flow/airports/lib/airports.ex
+++ 04_processing_collections_with_flow/airports/lib/airports.ex
@@ -10,12 +10,14 @@ defmodule Airports do
     airports_csv()
     |> File.read!()
     |> CSV.parse_string()
-    |> Enum.map(fn row -> %{
-      id: Enum.at(row, 0),
-      type: Enum.at(row, 2),
-      name: Enum.at(row, 3),
-      country: Enum.at(row, 8)
-    } end)
+    |> Enum.map(fn row ->
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
     |> Enum.reject(&(&1.type == "closed"))
     |> Enum.filter(&(&1.country == "BR"))
   end
@@ -25,12 +27,14 @@ defmodule Airports do
     airports_csv()
     |> File.stream!()
     |> CSV.parse_stream()
-    |> Stream.map(fn row -> %{
-      id: :binary.copy(Enum.at(row, 0)),
-      type: :binary.copy(Enum.at(row, 2)),
-      name: :binary.copy(Enum.at(row, 3)),
-      country: :binary.copy(Enum.at(row, 8))
-    } end)
+    |> Stream.map(fn row ->
+      %{
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
+      }
+    end)
     |> Stream.reject(&(&1.type == "closed"))
     |> Stream.filter(&(&1.country == "BR"))
     |> Enum.to_list()
@@ -42,12 +46,14 @@ defmodule Airports do
     |> File.stream!()
     |> CSV.parse_stream()
     |> Flow.from_enumerable()
-    |> Flow.map(fn row -> %{
-      id: :binary.copy(Enum.at(row, 0)),
-      type: :binary.copy(Enum.at(row, 2)),
-      name: :binary.copy(Enum.at(row, 3)),
-      country: :binary.copy(Enum.at(row, 8))
-    } end)
+    |> Flow.map(fn row ->
+      %{
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
+      }
+    end)
     |> Flow.reject(&(&1.type == "closed"))
     |> Flow.filter(&(&1.country == "BR"))
     |> Enum.to_list()
@@ -67,9 +73,119 @@ defmodule Airports do
         name: Enum.at(row, 3),
         country: Enum.at(row, 8)
       }
-     end)
+    end)
     |> Flow.reject(&(&1.type == "closed"))
     |> Flow.filter(&(&1.country == "BR"))
     |> Enum.to_list()
+  end
+
+  def open_airports_5() do
+    # tc: 111_280
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.reduce(
+      fn -> %{} end,
+      fn item, acc -> Map.update(acc, item.country, 1, &(&1 + 1)) end
+    )
+    |> Enum.to_list()
+  end
+
+  def open_airports_6() do
+    # doesn't work
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Enum.to_list()
+  end
+
+  def open_airports_7() do
+    # tc: 111_280
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.reduce(
+      fn -> %{} end,
+      fn item, acc -> Map.update(acc, item.country, 1, &(&1 + 1)) end
+    )
+    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Enum.to_list()
+    |> List.flatten()
+  end
+
+  def open_airports_8() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
+    airports_csv()
+    |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(window: window, key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done -> {events, acc}
+        {:every, 1000} -> {[], acc}
+      end
+    end)
+    |> Enum.sort(fn {_, a}, {_, b} -> a > b end)
+    |> Enum.take(10)
   end
 end
