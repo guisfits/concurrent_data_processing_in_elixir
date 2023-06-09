d7b82df3ec6bd82bed0356f83a180b1eecb5f641
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 15:18:46 -0300

    config bookings pipeline

--- 05_data-ingestion_pipelines_with_broadway/tickets/lib/tickets/application.ex
+++ 05_data-ingestion_pipelines_with_broadway/tickets/lib/tickets/application.ex
@@ -1,6 +1,4 @@
 defmodule Tickets.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,9 @@ defmodule Tickets.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Tickets.Worker.start_link(arg)
-      # {Tickets.Worker, arg}
+      BookingsPipeline
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Tickets.Supervisor]
     Supervisor.start_link(children, opts)
   end
