60c1619ebfaa4cbdffc32a9ed8685d4dcc56af4c
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 16:42:06 -0300

    implement handle_failed/2

--- 05_data-ingestion_pipelines_with_broadway/tickets/lib/bookings_pipeline.ex
+++ 05_data-ingestion_pipelines_with_broadway/tickets/lib/bookings_pipeline.ex
@@ -28,19 +28,20 @@ defmodule BookingsPipeline do
 
   def prepare_messages(messages, _context) do
     # * Parse data and convert to a map
-    messages = Enum.map(messages, fn message ->
-      Broadway.Message.update_data(message, fn data ->
-        [event, user_id] = String.split(data, ",")
-        %{event: event, user_id: user_id}
+    messages =
+      Enum.map(messages, fn message ->
+        Broadway.Message.update_data(message, fn data ->
+          [event, user_id] = String.split(data, ",")
+          %{event: event, user_id: user_id}
+        end)
       end)
-    end)
 
     users = Tickets.users_by_ids(Enum.map(messages, & &1.data.user_id))
 
     # * Put users in messages.
     Enum.map(messages, fn message ->
       Broadway.Message.update_data(message, fn data ->
-        user = Enum.find(users, & &1.id == data.user_id)
+        user = Enum.find(users, &(&1.id == data.user_id))
         Map.put(data, :user, user)
       end)
     end)
@@ -49,11 +50,24 @@ defmodule BookingsPipeline do
   def handle_message(_processor, message, _context) do
     %{data: %{event: event, user: user}} = message
 
-    # TODO: check for tickets availability
+    if Tickets.tickets_available?(event) do
+      Tickets.create_ticket(user, event)
+      Tickets.send_email(user)
+      IO.inspect(message, label: "message")
+    else
+      Broadway.Message.failed(message, "bookings-closed")
+    end
+  end
 
-    Tickets.create_ticket(user, event)
-    Tickets.send_email(user)
+  def handle_failed(messages, _context) do
+    messages
+    |> IO.inspect(label: "failed messages")
+    |> Enum.map(fn
+      %{status: {:failed, "bookings-closed"}} = message ->
+        Broadway.Message.configure_ack(message, on_failure: :reject)
 
-    IO.inspect(message, label: "message")
+      message ->
+        message
+    end)
   end
 end
