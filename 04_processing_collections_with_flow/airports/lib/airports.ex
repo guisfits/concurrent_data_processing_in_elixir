4dcb596777eee79ce4237ffb3b4dd58c964ef75e
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 11:50:30 -0300

    use flow to process csv

--- 04_processing_collections_with_flow/airports/lib/airports.ex
+++ 04_processing_collections_with_flow/airports/lib/airports.ex
@@ -6,6 +6,7 @@ defmodule Airports do
   end
 
   def open_airports_1() do
+    # tc: 1_218_463
     airports_csv()
     |> File.read!()
     |> CSV.parse_string()
@@ -17,10 +18,10 @@ defmodule Airports do
     } end)
     |> Enum.reject(&(&1.type == "closed"))
     |> Enum.filter(&(&1.country == "BR"))
-    |> Enum.sort_by(&(&1.name))
   end
 
   def open_airports_2() do
+    # tc: 987_995
     airports_csv()
     |> File.stream!()
     |> CSV.parse_stream()
@@ -32,6 +33,43 @@ defmodule Airports do
     } end)
     |> Stream.reject(&(&1.type == "closed"))
     |> Stream.filter(&(&1.country == "BR"))
+    |> Enum.to_list()
+  end
+
+  def open_airports_3() do
+    # tc: 692_616
+    airports_csv()
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row -> %{
+      id: :binary.copy(Enum.at(row, 0)),
+      type: :binary.copy(Enum.at(row, 2)),
+      name: :binary.copy(Enum.at(row, 3)),
+      country: :binary.copy(Enum.at(row, 8))
+    } end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.filter(&(&1.country == "BR"))
+    |> Enum.to_list()
+  end
+
+  def open_airports_4() do
+    # tc: 91_521
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+     end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.filter(&(&1.country == "BR"))
     |> Enum.to_list()
   end
 end
