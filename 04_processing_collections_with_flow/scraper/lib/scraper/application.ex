de8eca43d20d1d623f37979fa8b06e10a35dd5ff
Guilherme Camargo <guisfits@hotmail.com>
2023-06-09 14:57:57 -0300

    using flow to process scraper

---
+++ 04_processing_collections_with_flow/scraper/lib/scraper/application.ex
@@ -0,0 +1,16 @@
+defmodule Scraper.Application do
+  @moduledoc false
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      PageProducer,
+      PageConsumerSupervisor,
+      OnlinePageProducerConsumer
+    ]
+
+    opts = [strategy: :one_for_one, name: Scraper.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
